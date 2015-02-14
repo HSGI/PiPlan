8fa8087e199fc3655d2be2de59fad43c12610ca6
Christian Volkmann <ch.volkmann@gmail.com>
2015-02-14 14:57:04 +0100

    Added actual data to Data

--- input.py
+++ input.py
@@ -2,9 +2,34 @@ import json
 
 class Data:
 	def __init__(self):
-		self.data = []
+		pass
 	def load(self, path):
-		
+		self.data = {
+			"header": {
+				"date": "14.02.15",
+				"weekday": "Donnerstag",
+			},
+			"substitutes": [
+				{
+					"token": "KOK",
+					"lesson": 3,
+					"grade": "12",
+					"room": "C17",
+					"description": "Kock hahahahahaha"
+				},
+				{
+					"token": "KOK",
+					"lesson": 4,
+					"grade": "12",
+					"room": "C17",
+					"description": "KOK hahahahahaha"
+				}
+			],
+			"motd": [
+				"Message 1",
+				"Message -1"
+			]
+		}
 	def export(self, path):
 		f = open(path, "w")
 		f.write(json.dumps(path))
