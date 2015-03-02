cc19dafcaabf38aaa1f1dd2311699c1f3ad9ed6a
Christian Volkmann <pub.mrfreeman@gmail.com>
2015-03-02 21:35:14 +0100

    Cleanup

--- input.py
+++ input.py
@@ -1,86 +1,73 @@
 # -*- coding: utf-8 -*-
+
 import json
 
 class Data:
 	def __init__(self):
 		pass
+	
 	def load(self, path):
 		grabber = Grabber(path)
+		
 		self.data = {}
 		self.data["header"] = {}
 		self.data["header"]["weekday"] = grabber.weekday
 		self.data["header"]["date"] = grabber.date
+		
 		self.data["substitutes"] = []
-		for sub in grabber.substitutions:
-			asub = {}
-			asub["token"] = sub.token
-			asub["lesson"] = sub.lesson
-			asub["grade"] = sub.grade
-			asub["room"] = sub.room
-			asub["description"] = sub.description
-			self.data["substitutes"].append(asub)
+		for sub in grabber.substitutes:
+			tmp = {}
+			tmp["id"] = sub.id
+			tmp["lesson"] = sub.lesson
+			tmp["grade"] = sub.grade
+			tmp["room"] = sub.room
+			tmp["description"] = sub.description
+			self.data["substitutes"].append(tmp)
+			
 		self.data["motd"] = grabber.motd
-		"""self.data = {
-			"header": {
-				"date": "14.02.15",
-				"weekday": "Donnerstag",
-			},
-			"substitutes": [
-				{
-					"token": "KOK",
-					"lesson": 3,
-					"grade": "12",
-					"room": "C17",
-					"description": "Kock hahahahahaha"
-				},
-				{
-					"token": "KOK",
-					"lesson": 4,
-					"grade": "12",
-					"room": "C17",
-					"description": "KOK hahahahahaha"
-				}
-			],
-			"motd": [
-				"Message 1",
-				"Message -1"
-			]
-		}"""
+	
 	def export(self, path):
 		f = open(path, "w+")
 		f.write(json.dumps(self.data, ensure_ascii=False))
 		f.close()
+	
 
-class Substitution:
+class Substitute:
 	def __init__(self):
-		self.token = ""
+		self.id = ""
 		self.lesson = ""
 		self.grade = ""
 		self.room = ""
 		self.description = ""
-		
+	
+
 class Grabber:
 	def __init__(self, path):
 		f = open(path, "r")
+		
 		def getLine(f):
 			return f.readline()[:-1]
+		
 		line = getLine(f)
 		self.date = line[-10:]
+		
 		self.weekday = line[15:]
 		self.weekday = self.weekday[:-12].strip()
 		
-		getLine(f)
 		self.motd = []
+		getLine(f)
 		line = getLine(f)
 		while(not line.startswith("Es fehlen:")):
 			if(line):
 				self.motd.append(line)
 			line = getLine(f)
+		
 		offset = line.find("Vertretungen:")
 		nameType = "missing"
 		self.missing = []
 		self.hallMonitiors = []
-		self.substitutions = []
+		self.substitutes = []
+		
 		line = getLine(f)
 		while(len(line) > 3):
 			if(line.startswith("Pausenaufsichten:")):
@@ -93,33 +80,34 @@ class Grabber:
 					self.missing.append(name)
 				elif(nameType == "hallMonitiors"):
 					self.hallMonitiors.append(name)
-			substitutionStr = line[offset:]
-			substitution = Substitution()
 			
-			pos = substitutionStr.find(" ")
-			substitution.token = substitutionStr[:pos]
-			substitutionStr = substitutionStr[pos:].lstrip()
+			substr = line[offset:]
+			substitute = Substitute()
 			
-			pos = substitutionStr.find(" ")
-			substitution.lesson = substitutionStr[:pos]
-			substitutionStr = substitutionStr[pos:].lstrip()
+			posId = substr.find(" ")
+			substitute.id = substr[:posId]
+			substr = substr[posId:].lstrip()
 			
-			pos = substitutionStr.find(" ")
-			substitution.grade = substitutionStr[:pos]
-			substitutionStr = substitutionStr[pos:].lstrip()
+			posLesson = substr.find(" ")
+			substitute.lesson = substr[:posLesson]
+			substr = substr[posLesson:].lstrip()
 			
-			roomExisting = len(substitutionStr.split("  ")) > 1
-			if(roomExisting):
-				pos = substitutionStr.find(" ")
-				substitution.room = substitutionStr[:pos]
-				substitutionStr = substitutionStr[pos:].lstrip()
+			posGrade = substr.find(" ")
+			substitute.grade = substr[:posGrade]
+			substr = substr[posGrade:].lstrip()
 			
-			pos = substitutionStr.find(" ")
-			substitution.description = substitutionStr
+			roomExists = (len(substr.split("  ")) > 1)
+			if(roomExists):
+				posRoom = substr.find(" ")
+				substitute.room = substr[:posRoom]
+				substr = substr[posRoom:].lstrip()
 			
-			self.substitutions.append(substitution)
+			posDesc = substr.find(" ")
+			substitute.description = substr
+			
+			self.substitutes.append(substitute)
 			line = getLine(f)
-        		
+        	
 
 data = Data()
 data.load("plan0.txt")
